38615fb088d02d3082f40242a017f838e95e95f0
Rubuz <aterepin@yandex.ru>
2019-04-12 03:29:14 +0500

    Уроки JS

---
+++ 5 Type of inf/js.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+var number = 5;
+var string = 'Hello';
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'+9);
+
+let something;
+console.log(something)
+
+var person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp']; // Массив
+
+console.log(arr['0']) // Номерация начинается с 0
